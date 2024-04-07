5e10fe21a8b2931f45102438b12a786f03855d6b
Rafael Cecchin <contatorafaelcecchin@gmail.com>
2024-04-07 12:54:53 -0300

    feature: adicionar outro restaurante

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
+/* Restaurantes */
+const urlParams = new URLSearchParams(window.location.search);
+const restaurant = urlParams.get('restaurant');
+let logo = '';
+
+switch(restaurant) {
+    case 'arriba':
+        logo = '<img class="logo" src="./assets/img/arriba-logo.png" width="250" height="321" alt="Logo da Arriba Pizza"/>';
+        break;
+    default:
+        logo = '<img class="logo" src="./assets/img/suprema-logo.png" width="184" height="380" alt="Logo da Suprema Pizza"/>';
+        break;
+}
+
+document.querySelector('.page-1 .logo-container').innerHTML = logo;
+
+/* Etapas */
+
 // Page 1
 document.querySelector('.page-1 #acessar-cardapio').addEventListener('click', () => {
     Steps.goToNextStep();
 });
-
 // Page-2 -> roleta.js
-
 // Page-3
 function submitForm() {
     document.querySelector('#apelido-replace').textContent = document.querySelector('.questions #apelido').value;
@@ -12,7 +28,6 @@ function submitForm() {
     
     Steps.goToNextStep();
 }
-
 // Page 4
 document.querySelector('.page-4 #sim').addEventListener('click', () => {
     Steps.goToNextStep();
